1ae612e699966efd24940ec534f2715560b23780
GhaiPulkit <pulkit.ghai1501@gmail.com>
2025-10-17 23:30:25 -0400

    fix

--- apps/ui/src/components/modules/home/HomeView.tsx
+++ apps/ui/src/components/modules/home/HomeView.tsx
@@ -8,8 +8,8 @@ const HomeView: React.FC = () => {
                     {/* Banner */}
 
             <div className="px-4 py-8 w-full flex flex-col gap-2 bg-gradient-to-r from-[rgba(223,237,206,1)] to-[rgba(213,237,229,1)]">
-                <h1>India's Leading B2B Pharmaceutical Marketplace</h1>
-                <p className='text-[grey]'>Connect with verified manufacturers, traders, and distributors. Find quality pharmaceutical products or offer your manufacturing services.
+                <h1>India&apos;s Leading B2B Pharmaceutical Marketplace</h1>
+                <p className="text-[grey]">Connect with verified manufacturers, traders, and distributors. Find quality pharmaceutical products or offer your manufacturing services.
                 </p>
                 <div className="action-bar flex gap-2">
                     <Button >Browse manufacturers</Button>
